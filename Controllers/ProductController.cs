6d397ea46140ecf5f5b0ed926c6ef80f697aa939
Delatorrea <emerson@delatorre.dev>
2021-08-05 22:09:22 -0300

    Finalizado a API

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+  [Route("products")]
+  public class ProductController : ControllerBase
+  {
+    [HttpGet]
+    [Route("")]
+    [AllowAnonymous]
+    public async Task<ActionResult<List<Product>>> Get(
+      [FromServices] DataContext context
+    )
+    {
+      var products = await context
+            .Products
+            .Include(x => x.Category)
+            .AsNoTracking()
+            .ToListAsync();
+
+      return Ok(products);
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public async Task<ActionResult<Product>> GetById(
+      int id,
+      [FromServices] DataContext context
+    )
+    {
+      var products = await context
+            .Products
+            .Include(x => x.Category)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+      return Ok(products);
+    }
+
+    [HttpGet] // products/categories/1
+    [Route("categories/{id:int}")]
+    [AllowAnonymous]
+    public async Task<ActionResult<List<Product>>> GetByCategory(
+      int id,
+      [FromServices] DataContext context
+    )
+    {
+      var products = await context
+            .Products
+            .Include(x => x.Category)
+            .AsNoTracking()
+            .Where(x => x.CategoryId == id)
+            .ToListAsync();
+
+      return Ok(products);
+    }
+
+    [HttpPost]
+    [Route("")]
+    [Authorize(Roles = "employee")]
+    public async Task<ActionResult<Product>> Post(
+      [FromBody] Product model,
+      [FromServices] DataContext context
+    )
+    {
+      if (!ModelState.IsValid)
+        return BadRequest(ModelState);
+
+      try
+      {
+        context.Products.Add(model);
+        await context.SaveChangesAsync();
+        return Ok(model);
+      }
+      catch
+      {
+        return BadRequest(new { message = "Não foi possível criar o produto" });
+      }
+    }
+
+    [HttpPut]
+    [Route("{id:int}")]
+    [Authorize(Roles = "manager")]
+    public async Task<ActionResult<Product>> Put(
+      int id,
+      [FromBody] Product model,
+      [FromServices] DataContext context
+    )
+    {
+      // Verifica se o ID informado é o mesmo do modelo
+      if (id != model.Id)
+        return NotFound(new { Message = "Produto não encontrado" });
+
+      // Verifica se os dados são válidos
+      if (!ModelState.IsValid)
+        return BadRequest(ModelState);
+
+      try
+      {
+        context.Entry<Product>(model).State = EntityState.Modified;
+        await context.SaveChangesAsync();
+        return Ok(model);
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        return BadRequest(new { message = "Este registro já foi atualizado" });
+      }
+      catch (Exception)
+      {
+        return BadRequest(new { message = "Não foi possível atualizar o produto" });
+      }
+    }
+
+    [HttpDelete]
+    [Route("{id:int}")]
+    [Authorize(Roles = "manager")]
+    public async Task<ActionResult<Product>> Delete(
+      int id,
+      [FromServices] DataContext context
+    )
+    {
+      var product = await context.Products.FirstOrDefaultAsync(x => x.Id == id);
+      if (product == null)
+        return NotFound(new { message = "Produto não encontrado" });
+
+      try
+      {
+        context.Products.Remove(product);
+        await context.SaveChangesAsync();
+        return Ok(new { message = "Produto removido com sucesso" });
+      }
+      catch (Exception)
+      {
+        return BadRequest(new { message = "Não foi possível remover o produto" });
+      }
+    }
+  }
+}
---
+++ Settings.cs
@@ -0,0 +1,7 @@
+namespace Shop
+{
+  public static class  Settings
+  {
+    public static string Secret = "5624c56ee493c04b1c8cb953799c49d8948043b505ae21ba05c95e61bf36bcccf96c81caeded4161f0601abbbc44e7d7435da8fef2ea019897d59505429a0259";
+  }
+}
